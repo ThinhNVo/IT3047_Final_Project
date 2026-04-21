54570ee3973aa1e028d8289bb6d8f959227afe42
Opal Shah <shahol@mail.uc.edu>
2026-04-20 21:05:11 -0400

    added Error Messages to Members.cs & "About" tab with heading and body

---
+++ IT3047_Final_Project/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IT3047_Final_Project.Models;
+
+namespace IT3047_Final_Project.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly MembersContext _context;
+
+        public AboutController(MembersContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var members = _context.Members.ToList();
+            return View(members);
+        }
+    }
+}
--- IT3047_Final_Project/Models/Members.cs
+++ IT3047_Final_Project/Models/Members.cs
@@ -1,12 +1,25 @@
-﻿namespace IT3047_Final_Project.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT3047_Final_Project.Models
 {
     public class Members
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter the member's name.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter the member's major.")]
         public string Major { get; set; }
+
+        [Required(ErrorMessage = "Please enter the member's bio.")]
         public string Bio { get; set; }
+
+        [Required(ErrorMessage = "Please enter the member's age.")]
+        [Range(1, 5, ErrorMessage = "Age must be between 1 and 50.")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter the member's dream job.")]
         public string DreamJob { get; set; }
 
     }
